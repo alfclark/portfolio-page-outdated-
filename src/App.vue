6bee85a677ab0875508b06cb1022f3730f2e5586
Alfredo Clark <alfredoclarkcardenas@gmail.com>
2022-03-31 18:36:14 -0700

    Animations added

--- src/App.vue
+++ src/App.vue
@@ -57,6 +57,49 @@ export default {
     transform: translateY(0);
   }
 }
+@keyframes entrance1 {
+  0% {
+    animation-timing-function: ease-in;
+    opacity: 0;
+    transform: translateY(-250px);
+  }
+
+  38% {
+    animation-timing-function: ease-out;
+    opacity: 1;
+    transform: translateY(0);
+  }
+
+  55% {
+    animation-timing-function: ease-in;
+    transform: translateY(-65px);
+  }
+
+  72% {
+    animation-timing-function: ease-out;
+    transform: translateY(0);
+  }
+
+  81% {
+    animation-timing-function: ease-in;
+    transform: translateY(-28px);
+  }
+
+  90% {
+    animation-timing-function: ease-out;
+    transform: translateY(0);
+  }
+
+  95% {
+    animation-timing-function: ease-in;
+    transform: translateY(-8px);
+  }
+
+  100% {
+    animation-timing-function: ease-out;
+    transform: translateY(0);
+  }
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   color: white;
