35a5771c6954c2c6f7f970401f32dbf694693004
Alfredo Clark <alfredoclarkcardenas@gmail.com>
2022-03-31 18:29:33 -0700

    Workbench design and link to components

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,66 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <NavBar />
+  <LandingScreen />
+  <AboutMe />
+  <FooterView />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavBar from "./components/NavBar.vue";
+import LandingScreen from "./components/LandingScreen.vue";
+import AboutMe from "./components/AboutMe.vue";
+
+import FooterView from "./components/FooterView.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    NavBar,
+    LandingScreen,
+    AboutMe,
+    FooterView,
+  },
+};
 </script>
 
 <style>
+/* CUSTOM SCROLLBAR */
+::-webkit-scrollbar-track {
+  background-color: #141c3a1a;
+}
+
+::-webkit-scrollbar {
+  width: 12px;
+  background-color: #141c3a;
+}
+
+::-webkit-scrollbar-thumb {
+  background-color: var(--purple);
+}
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+:root {
+  --purple: #783ad6;
+}
+/* ANIMATIONS */
+@keyframes bounce1 {
+  0% {
+    transform: translateY(0);
+  }
+  50% {
+    transform: translateY(-20px);
+  }
+  100% {
+    transform: translateY(0);
+  }
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  color: white;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background: 50% / contain url(@/assets/universe.png) #141c3a;
 }
 </style>
