ca62e43a5f9145f3ac7ffbc61f12e7057b154b89
Alfredo Clark <alfredoclarkcardenas@gmail.com>
2022-04-06 00:53:16 -0500

    Go to top button added

---
+++ src/components/GoTop.vue
@@ -0,0 +1,33 @@
+<template>
+  <i @click="goTop" class="fa-solid fa-arrow-up"></i>
+</template>
+
+<script>
+export default {
+  methods: {
+    goTop() {
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+      });
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.fa-arrow-up {
+  position: fixed;
+  right: 5%;
+  bottom: 5%;
+  cursor: pointer;
+  animation: bounce1 5s infinite;
+  color: #fff;
+  font-size: 2rem;
+  z-index: 3;
+}
+@media screen and (max-width: 900px) {
+}
+</style>
